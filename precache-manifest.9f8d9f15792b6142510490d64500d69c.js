201d6fab2b7578fad1d1425ad1c77ca531c4af59
KRMaendo <40663381+KRMaendo@users.noreply.github.com>
2020-09-03 17:01:51 +0200

    Updates

---
+++ precache-manifest.9f8d9f15792b6142510490d64500d69c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc1dc270b88ab5915978440616b15f74",
+    "url": "/react-monster-rolodex/index.html"
+  },
+  {
+    "revision": "65168f08d8e11340f2b7",
+    "url": "/react-monster-rolodex/static/css/main.11f6c9c5.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/react-monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65168f08d8e11340f2b7",
+    "url": "/react-monster-rolodex/static/js/main.a0e288d2.chunk.js"
+  },
+  {
+    "revision": "e5eb423b2a2cc1d9f46f",
+    "url": "/react-monster-rolodex/static/js/runtime-main.856af338.js"
+  }
+]);
